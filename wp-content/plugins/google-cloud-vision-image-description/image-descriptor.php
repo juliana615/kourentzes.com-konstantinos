92b74cd43887917d6d3b2d59b5636746a93bdd33
juliana615 <albrecht.2004.j@gmail.com>
2023-09-05 19:52:47 -0700

    Add google vision composer|Update image-descriptor.php

--- wp-content/plugins/google-cloud-vision-image-description/image-descriptor.php
+++ wp-content/plugins/google-cloud-vision-image-description/image-descriptor.php
@@ -1,10 +1,9 @@
 <?php
 // compose update google/vision
-require '../../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 
 use Google\Cloud\Core\ServiceBuilder;
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-
 
 // Init google cloud vision API
 $config = [
@@ -12,55 +11,66 @@ $config = [
     'projectId' => 'konstantinos-kourentzes-com',
 ];
 
-//$imageAnnotator = new ImageAnnotatorClient($config);
-$imageAnnotator = new \Google\Cloud\Vision\V1\ImageAnnotatorClient($config);
+try {
+    //$imageAnnotator = new ImageAnnotatorClient($config);
+    // $imageAnnotator = new Google\Cloud\Vision\V1\ImageAnnotatorClient($config);
+    $imageAnnotator = new ImageAnnotatorClient();
 
-// main fn
-function update_image_descriptions($image_id, $image_file, $imageAnnotator) {
-    $image = file_get_contents($image_file);
-    $response = $imageAnnotator->labelDetection($image);
-    $labels = $response->getLabelAnnotations();
-    $image_description = [];
+    // main fn
+    function update_image_descriptions($image_id, $image_file, $imageAnnotator) {
+        $image = file_get_contents($image_file);
+        $response = $imageAnnotator->labelDetection($image);
+        $labels = $response->getLabelAnnotations();
+        $image_description = [];
 
-    
-    foreach ($labels as $label) {
-        $image_description[] = $label->getDescription();
-    }
+        
+        foreach ($labels as $label) {
+            $image_description[] = $label->getDescription();
+        }
 
-    /*
-    Alternate Image description is -> _wp_attachment_image_alt
-    Call through WP is -> $image_alt = get_post_meta( $image->id, '_wp_attachment_image_alt', true);
-    */
-    if ($image_id) {
-        $updated = update_post_meta($image_id, '_wp_attachment_image_alt', implode(', ', $image_description));
-        if ($updated) {
-            echo "Updated alternate text for image: {$image_file}" . PHP_EOL;
-        } else {
-            echo "Failed to update alternate text for image: {$image_file}" . PHP_EOL;
+        /*
+        Alternate Image description is -> _wp_attachment_image_alt
+        Call through WP is -> $image_alt = get_post_meta( $image->id, '_wp_attachment_image_alt', true);
+        */
+        if ($image_id) {
+            $updated = update_post_meta($image_id, '_wp_attachment_image_alt', implode(', ', $image_description));
+            if ($updated) {
+                echo "Updated alternate text for image: {$image_file}" . PHP_EOL;
+            } else {
+                echo "Failed to update alternate text for image: {$image_file}" . PHP_EOL;
+            }
         }
     }
+
+    //change to do multiples from selected
+    $selectedImageIds = isset($_POST['image_ids']) ? $_POST['image_ids'] : array();
+
+    /* 
+        
+        Constants:
+        WP_CONTENT_DIR  // no trailing slash, full paths only
+        WP_CONTENT_URL  // full url 
+        WP_PLUGIN_DIR  // full path, no trailing slash
+        WP_PLUGIN_URL  // full url, no trailing slash
+        
+    */
+    if ( !defined('WP_CONTENT_DIR') )
+        define( 'WP_CONTENT_DIR', '../../' );
+    $media_directory = WP_CONTENT_DIR . '/uploads/';
+
+
+    echo("before foreach: ".$selectedImageIds);
+    foreach ($selectedImageIds as $image_id) {
+        $image_file = get_attached_file($image_id);
+        echo("before update_image_descriptions");
+        update_image_descriptions($image_id, $image_file, $imageAnnotator);
+        echo("after update_image_descriptions");
+
+    }
+    echo("after foreach");
+
+    // clean up 
+    $imageAnnotator->close();
+} catch(Exception $e) {
+    echo $e->getMessage();
 }
-
-//change to do multiples from selected
-$selectedImageIds = isset($_POST['image_ids']) ? $_POST['image_ids'] : array();
-
-/* 
-    
-    Constants:
-    WP_CONTENT_DIR  // no trailing slash, full paths only
-    WP_CONTENT_URL  // full url 
-    WP_PLUGIN_DIR  // full path, no trailing slash
-    WP_PLUGIN_URL  // full url, no trailing slash
-    
-*/
-
-$media_directory = WP_CONTENT_DIR . '/uploads/';
-
-foreach ($selectedImageIds as $image_id) {
-    $image_file = get_attached_file($image_id);
-
-    update_image_descriptions($image_id, $image_file, $imageAnnotator);
-}
-
-// clean up 
-$imageAnnotator->close();
